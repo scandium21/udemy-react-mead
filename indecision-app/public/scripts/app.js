9e380dcd17cb21d67b0d6d636322f06a833ca813
SC <siliang.chang@gmail.com>
2019-10-31 14:14:07 -0600

    conditional rendering in JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,23 +1,96 @@
-'use strict';
+"use strict";
 
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-console.log('App.js is running');
+console.log("App.js is running");
+
+// practice
+// create app object title/subtitle
+// use title/subtitle in the template
+// render template
+var app = {
+  title: "Indecision app",
+  subtitle: "Put your life in the hands of a computer!",
+  options: ["one", "two"]
+};
+
+// practice:
+// only render the subtitle (and p tag) if subtitle exists - logical and operator
+// conditionally render new p tag - if options.length > 0 "Here are your options" "no options"
 
 // JSX - Javascript XML
 var template = React.createElement(
-  'div',
+  "div",
   null,
   React.createElement(
-    'h1',
+    "h1",
     null,
-    'Indecision App'
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
   ),
   React.createElement(
-    'p',
+    "p",
     null,
-    'This is some info'
+    app.options.length > 0 ? "Here are your options" : "No options"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item One"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item Two"
+    )
   )
 );
-var appRoot = document.querySelector('#app');
+
+var appRoot = document.querySelector("#app");
+
+var user = {
+  name: "Nancy",
+  age: 47,
+  loc: "New York"
+};
+var userName = "Lucy";
+var userAge = 27;
+var userLoc = "Calgary";
+
+var getLocation = function getLocation(loc) {
+  if (loc) return React.createElement(
+    "p",
+    null,
+    "Location: ",
+    loc
+  );
+};
+// create a templateTwo var JSX expression
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name ? user.name : "Anonymous"
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    "p",
+    null,
+    "Age: ",
+    user.age
+  ),
+  getLocation(user.loc),
+  undefined,
+  true,
+  false,
+  null
+);
 
 ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,61 @@
 // babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
-console.log('App.js is running');
+console.log("App.js is running");
+
+// practice
+// create app object title/subtitle
+// use title/subtitle in the template
+// render template
+let app = {
+  title: "Indecision app",
+  subtitle: "Put your life in the hands of a computer!",
+  options: ["one", "two"]
+};
+
+// practice:
+// only render the subtitle (and p tag) if subtitle exists - logical and operator
+// conditionally render new p tag - if options.length > 0 "Here are your options" "no options"
 
 // JSX - Javascript XML
 let template = (
   <div>
-    <h1>Indecision App</h1>
-    <p>This is some info</p>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? `Here are your options` : `No options`}</p>
+    <ol>
+      <li>Item One</li>
+      <li>Item Two</li>
+    </ol>
   </div>
 );
-let appRoot = document.querySelector('#app');
+
+let appRoot = document.querySelector("#app");
+
+let user = {
+  name: "Nancy",
+  age: 47,
+  loc: "New York"
+};
+let userName = "Lucy";
+let userAge = 27;
+let userLoc = "Calgary";
+
+const getLocation = loc => {
+  if (loc) return <p>Location: {loc}</p>;
+};
+// create a templateTwo var JSX expression
+let templateTwo = (
+  <div>
+    {/* what goes inside the {any js expression}  */}
+    <h1>{user.name ? user.name : "Anonymous"}</h1>
+    {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+    {getLocation(user.loc)}
+    {/* the following will not render into anything */}
+    {undefined}
+    {true}
+    {false}
+    {null}
+    {/* true && (2nd cond) --> will use the 2nd cond if it's true */}
+  </div>
+);
 
 ReactDOM.render(template, appRoot);
